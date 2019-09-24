2af4a15ec3b5477c70c4aacf31ca8d83508ad2c7
Max Lee <maxolee23@gmail.com>
2019-09-24 22:09:40 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,51 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select {|painting| painting.artist == self}
+  end
+
+  def galleries
+    paintings.map do |painting|
+      painting.gallery
+    end
+  end
+
+  def cities
+    galleries.map do |gallery|
+      gallery.city
+    end
+  end
+
+  def self.total_experience
+    total = 0
+    self.all.each do |artist|
+      total += artist.years_experience
+    end
+    total
+  end
+
+  def self.most_prolific
+    self.all.max_by do |artist|
+      artist.paintings.length / artist.years_experience
+    end
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,24 @@
 require_relative '../config/environment.rb'
 
 
+a1 = Artist.new("Bob", 20)
+a2 = Artist.new("Morrie", 15)
+a3 = Artist.new("Arthur", 12)
+a4 = Artist.new("Andrew", 7)
 
+g1 = Gallery.new("Glassell", "Houston")
+g2 = Gallery.new("Smith", "Austin")
+g3 = Gallery.new("Sky", "LA")
+g4 = Gallery.new("Lee", "New York")
+
+p1 = Painting.new("Starry Night", 1000, a1, g1)
+p2 = Painting.new("Waterfall", 2500, a1, g2)
+p3 = Painting.new("Forest Fire", 1500, a2, g2)
+p4 = Painting.new("Flatiron", 3000, a2, g2)
+p5 = Painting.new("Volcano", 1050, a3, g3)
+p6 = Painting.new("Cheese", 500, a3, g4)
+p7 = Painting.new("Food", 360, a4, g4)
+p8 = Painting.new("Pizza", 700, a4, g4)
 
 binding.pry
 
